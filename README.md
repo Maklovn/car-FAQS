96882a7adbdef731ad7cbafd9c9ed0c05f08705f
Maklovn <137200003+Maklovn@users.noreply.github.com>
2023-09-11 08:25:33 -0600

    Histograms and Scaterplot deployed on web app successfully

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # car-FAQS
 All about cars
+# Car Data Analysis with Streamlit
+
+This Streamlit app allows you to explore and visualize data related to cars. You can analyze car prices, mileage, and more using the interactive visualizations provided. 
+
+## Getting started
+
+To run this streamlit app on your local machine, follow the steps below:
+
+1. Clone the repository to your local machine:
+
+'''bash 
+git clone https://github.com/Maklovn/car-FAQS.git
--- app.py
+++ app.py
@@ -3,16 +3,49 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px 
 
-#"Streamlit App Title"
+# Streamlit App Header
+st.title('Vehicle Data Analysis')
 
-st.title("Streamlit Web Application")
+# Load the cleaned data from the CSV file under df
+df = pd.read_csv('vehicles_us.csv')
 
-#Load Data
-data = pd.read_csv("vehicles_us.csv")
+# Display the dataset or a sample of it
+st.subheader('Dataset Preview')
+st.write(df.head())  # Display the first few rows of the dataset
 
-#Display Data
-st.write("Displaying Data:")
-st.dataframe(data)
-#Create Plot 
-fig = px.scatter (data, title="Scatter Plot")
-st.plotly_chart(fig)
+# Checkbox to toggle the visibility of the histogram
+show_histogram_price = st.checkbox('Show price Histogram')
+
+# Conditional rendering of the first histogram 
+if show_histogram_price:
+    # create a combined histogram for price
+    fig_price = px.histogram(df, x='price', color='condition', marginal='rug',
+                   hover_data=['model_year'], title="Price based on Condition")
+    st.subheader('Histogram')
+    st.plotly_chart(fig_price, use_container_width=True)
+
+# Create a combined histogram 
+fig = px.histogram(df, x='price', color='odometer', marginal='rug',
+                   hover_data=['model_year'], title='Price Based on Odometer')
+
+# Checkbox toggle the visibility of the second histogram 
+show_histogram_odometer = st.checkbox('Show Odometer Histogram')
+
+# Conditional rednering of the second histogram 
+if show_histogram_odometer:
+    # Create a combined histogram
+    fig_odometer = px.histogram(df, x='price', color='odometer', marginal='rug',
+                                hover_data=['model_year'], title='Price based on Odometer')
+   
+    # Display the second histogram 
+    st.subheader('Odometer Histogram')
+    st.plotly_chart(fig_odometer, use_container_width=True)
+
+# Checkbox to toggle teh visibility of the scatter plot
+show_scatter_plot = st.checkbox('Show Scatter Plot')
+
+# Conditional rendering of the scatter plot
+if show_scatter_plot:
+    # Display the scatter plot 
+    st.subheader('Scatter Plot')
+    st.plotly_chart(fig, use_container_width=True) # Use st.plotly_chart for the scatter plot
