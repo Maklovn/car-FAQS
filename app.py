193b2834e0e01fbf7db04959811f7040e869fbb1
Maklovn <137200003+Maklovn@users.noreply.github.com>
2023-09-19 11:28:30 -0600

    fixed app.py

--- app.py
+++ app.py
@@ -16,36 +16,40 @@ st.write(df.head())  # Display the first few rows of the dataset
 # Checkbox to toggle the visibility of the histogram
 show_histogram_price = st.checkbox('Show price Histogram')
 
-# Conditional rendering of the first histogram 
+# Conditional rendering of the first histogram
 if show_histogram_price:
-    # create a combined histogram for price
+    # Create a combined histogram for price
     fig_price = px.histogram(df, x='price', color='condition', marginal='rug',
                    hover_data=['model_year'], title="Price based on Condition")
     st.subheader('Histogram')
     st.plotly_chart(fig_price, use_container_width=True)
 
-# Create a combined histogram 
-fig = px.histogram(df, x='price', color='odometer', marginal='rug',
-                   hover_data=['model_year'], title='Price Based on Odometer')
-
-# Checkbox toggle the visibility of the second histogram 
+# Define show_histogram_odometer here (before usage)
 show_histogram_odometer = st.checkbox('Show Odometer Histogram')
 
-# Conditional rednering of the second histogram 
+# Conditional rendering of the second histogram (group by model_year and fill by median)
 if show_histogram_odometer:
-    # Create a combined histogram
-    fig_odometer = px.histogram(df, x='price', color='odometer', marginal='rug',
-                                hover_data=['model_year'], title='Price based on Odometer')
+    # Group by 'model_year' and fill missing values with median
+    df_grouped_by_year = df.groupby('model_year').transform(lambda x: x.fillna(x.median()))
+
+    # Create a combined histogram for 'price' based on 'odometer'
+    fig_odometer = px.histogram(df_grouped_by_year, x='price', color='odometer', marginal='rug',
+                                title='Price based on Odometer (Median Fill)')
    
     # Display the second histogram 
-    st.subheader('Odometer Histogram')
+    st.subheader('Odometer Histogram (Median Fill)')
     st.plotly_chart(fig_odometer, use_container_width=True)
 
-# Checkbox to toggle teh visibility of the scatter plot
+# Checkbox to toggle the visibility of the scatter plot
 show_scatter_plot = st.checkbox('Show Scatter Plot')
 
 # Conditional rendering of the scatter plot
 if show_scatter_plot:
+    # Create a scatter plot
+    fig_scatter = px.scatter(df, x='model', y='model_year', color='condition', title='Make, year and condition')
+
     # Display the scatter plot 
     st.subheader('Scatter Plot')
-    st.plotly_chart(fig, use_container_width=True) # Use st.plotly_chart for the scatter plot
+    st.plotly_chart(fig_scatter, use_container_width=True)
+
+
